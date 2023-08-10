b48a836576a119f8ffb72fc9e95d16bf98f882bf
Sistemshdortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2023-08-10 17:11:45 +0200

    just details

--- src/Components/LanguageDropDown.js
+++ src/Components/LanguageDropDown.js
@@ -1,3 +1,5 @@
+import '../Style/LanguageDropDown.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const LanguageDropDown = (props) => {
@@ -8,7 +10,14 @@ const LanguageDropDown = (props) => {
     };
 
     return (
-        <button onClick={clickHandle}>{props.children}</button>
+        <div>
+            {/* <button onClick={clickHandle} className='languageButton'>{props.children}</button> */}
+            <select onChange={clickHandle} className="languageButton" data-width="fit">
+                <option data-content='<span class="flag-icon flag-icon-us"></span> English'>Español</option>
+                <option data-content='<span class="flag-icon flag-icon-es"></span> Español'>English</option>
+            </select>
+        </div>
+
     )
 }
 
