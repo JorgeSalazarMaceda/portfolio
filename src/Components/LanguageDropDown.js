6369d94a7395a2f2fa88a54e50ebd8ceffa0aaba
Sistemshdortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2023-08-17 01:16:02 +0200

    new language added

--- src/Components/LanguageDropDown.js
+++ src/Components/LanguageDropDown.js
@@ -1,24 +1,23 @@
-import '../Style/LanguageDropDown.css'
+import '../Style/LanguageDropDown.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 const LanguageDropDown = (props) => {
 
-    const clickHandle = () => {
-        const newLanguage = props.settings.language === 'es' ? 'en' : 'es';
+    const clickHandle = (event) => {
+        const newLanguage = event.target.value;
         props.settings.changeLanguage(newLanguage);
+        console.log(newLanguage)
     };
 
     return (
         <div>
-            {/* <button onClick={clickHandle} className='languageButton'>{props.children}</button> */}
             <select onChange={clickHandle} className="languageButton" data-width="fit">
-                <option data-content='<span class="flag-icon flag-icon-us"></span> English'>Español</option>
-                <option data-content='<span class="flag-icon flag-icon-es"></span> Español'>English</option>
+                <option value="es">Español</option>
+                <option value="en">English</option>
+                <option value="ja">Japan</option>
             </select>
         </div>
+    );
+};
 
-    )
-}
-
-export default LanguageDropDown
+export default LanguageDropDown;
